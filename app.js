618b0e6f476207a4a1d429f51f6d870fd647debb
Shimei Wong <wshimei@gmail.com>
2017-02-12 18:41:19 +0800

    successful connection on localhost

--- app.js
+++ app.js
@@ -4,6 +4,39 @@ const port = process.env.PORT || 4000
 
 const todosControl = require('./controllers/todos_controller.js')
 const TodoSchema = require('./models/todo')
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+  res.send('Hello Todo List!')
+})
+
+app.listen(port, function () {
+  console.log('Express Test is running on ' + port)
+})
+
+app.get('/todos', function (req, res) {
+  TodoSchema.find({}, function (err, foundTodo) {
+    if (err) {
+      console.error(err)
+      return
+    }
+    res.render('./todos/index', {
+      allTodos: foundTodo
+    })
+  })
+})
+
+app.get('/todos/:_id', function (req, res) {
+  TodoSchema.findOne({id: req.params.id}, function (err, oneTodo) {
+    if (err) {
+      console.error(err)
+      return
+    }
+    res.render('./todos/show', {
+      todo: oneTodo
+    })
+  })
+})
 
 const readline = require('readline')
 const rl = readline.createInterface(process.stdin, process.stdout)
